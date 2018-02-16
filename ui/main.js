3ae542564c42d7edf3d5c66b2ff98ac2c36f28dd
akash3699 <31035322+akash3699@users.noreply.github.com>
2018-02-16 19:17:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,13 +37,27 @@ var nameInput = document.getElementById('name');
 var name=nameInput.value;
 var submit =document.getElementById('submit_btn');
 submit.onclick=function(){
+    var request = new XMLHttpRequest();
     
-    var names= ['name1','name2','name3','name4'];
+    request.onreadystatechange = function (){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var names= request.responceText;
+                names=JSON.parce(names);
     var list='';
     for(var i=0;i<names.length;i++){
         list +='<li>'+names[i]+'</li>';
      }
      
     var ul= document.getElementById('namelist');
-    ul.innerHTML=list;    
+    ul.innerHTML=list;
+            }
+        }
+        
+        
+    };
+    request.open('GET','http://akash3699.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+    
+        
 };
