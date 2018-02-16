b7776c69fc8f3fada16eed7f5eed13fefe32cfca
akash3699 <31035322+akash3699@users.noreply.github.com>
2018-02-16 16:10:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/ui/banner.jpg', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send('this article one is serverd here');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function (req, res) {
